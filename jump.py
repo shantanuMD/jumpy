1992f5e2cb043736c77ce16370488770c5506b7d
shantanuMD <98452964+shantanuMD@users.noreply.github.com>
2022-01-27 15:52:37 +0530

    Create jump.py

---
+++ jump.py
@@ -0,0 +1,9 @@
+# my-platformer-game 
+
+##Goal 
+
+Get the hero to the flag while avoiding monsters. Clear all three levels and you win the game 
+
+##Controls
+
+use the arrows to run and space to jump
